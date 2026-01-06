2ca09646c9401169712f6e70d13ab5c9b19544ab
Keyannish <briansismundo@gmail.com>
2026-01-06 15:31:51 +0800

    FixWebSocketcode 01/6/2026

--- ws/websocket_server.js
+++ ws/websocket_server.js
@@ -12,7 +12,26 @@ console.log("WebSocket server running on /ws");
 wss.on("connection", (ws) => {
   console.log("Client connected");
 
-  ws.on("message", (msg) => {
-    console.log("Received:", msg.toString());
+  ws.on("message", (message) => {
+    let data;
+
+    try {
+      data = JSON.parse(message.toString());
+      console.log("Received:", data);
+    } catch (e) {
+      console.error("Invalid JSON:", message.toString());
+      return;
+    }
+
+    // 🔴 BROADCAST TO ALL CONNECTED CLIENTS
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
   });
 });
