8436662433f843c4a0216b59b93ef049726d2091
Keyannish <briansismundo@gmail.com>
2026-02-27 23:35:01 +0800

    directory fix

--- dashboard/search_events.php
+++ dashboard/search_events.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-require_once __DIR__ . '/db_connect.php';
+require_once __DIR__ . '/../db_connect.php';
 
 try {
     $dbName = $pdo->query("SELECT current_database()")->fetchColumn();
